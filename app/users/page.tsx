fb2e7423441ee996779c7c316d4c0152c67a28da
miradil5656 <miradild404@gmail.com>
2023-09-29 11:34:33 +0900

    fetching

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
 
-const Users = () => {
+
+interface User{
+    id: number 
+    name: string 
+    email :string
+}
+
+const Users =async () => {
+    //fetching 
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users :User[] = await res.json()
+
   return (
-    <div>Users</div>
+    <>
+    <div className="flex flex-col w-[80%]  justify-between items-center p-4 m-auto">
+        <h1 className="text-3xl mb-8">Users</h1>
+        <ul className="text-2xl flex flex-col gap-4">
+            {users.map((user,index)=>(<li key={index}>{user.id} . {user.name}</li>))}
+        </ul>
+    </div>
+        
+    </>
   )
 }
 
